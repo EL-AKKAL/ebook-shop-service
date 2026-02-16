165ba87f98bc5365fdf68c4c71dea4ecc82aede4
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-02-16 16:44:49 +0000

    implement api endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,2 +1,12 @@
 <?php
+use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Ebook Shop Service is running.',
+        'api routes' => [
+            '/books' => 'GET : get paginated list of books',
+            '/books/tags' => 'GET : fetch available tags',
+        ],
+    ]);
+});
